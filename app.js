191c8d4c5915f483140ce8f74a93e4c8fe8b2f8e
Jorell Linsangan <jorell.linsangan@gmail.com>
2016-03-03 11:01:21 -0600

    Switched the application from a purely client side application to an application that uses Node.js

--- app.js
+++ app.js
@@ -1,31 +1,58 @@
-(function() {
-	var app = angular.module('umcc', []);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-	app.controller('WallController', function() {
-		this.routes = [
-			{
-				name: "Trump Tower",
-				grade: "12",
-				description: "Red route on the left of the overhang.",
-				setter: "Sean S."
-			},
-			{
-				name: "Green Willy",
-				grade: "11",
-				description: "Green crimpy route on over hang. Start with crimp and pocket with a heel hook",
-				setter: "Jorell L."
-			}
-		];
-	});
+var routes = require('./routes/index');
 
-	app.controller("RouteController", function() {
-		this.route = {};
+var app = express();
 
-		this.addRoute = function(wall) {
-			problem.routes.push(this.route);
-			this.route = {};
-		};
-	});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-	
-})();
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', routes);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
